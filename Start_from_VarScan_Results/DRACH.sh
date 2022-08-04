128b63c6a93c90213c2a4933ee16ef850ae8ea82
Source Codes for Computational and Theoretical Life Science <yongp@outlook.com>
2022-08-03 21:29:43 -0500

    Delete Start_from_VarScan_Results directory

--- Start_from_VarScan_Results/DRACH.sh
+++
@@ -1,6 +0,0 @@
-## conserved motif DRACH (D=G/A/U, R=G/A, H=A/U/C) 
-
-cat FP.fasta | grep -iP "^[AGT][AG]AC[ACT]" | wc -l  >> number.DRACH.txt
-cat TP.fasta | grep -iP "^[AGT][AG]AC[ACT]" | wc -l  >> number.DRACH.txt
-
-
--- Start_from_VarScan_Results/ToFasta.sh
+++
@@ -1,4 +0,0 @@
-bedtools getfasta -fo TP.fasta    -name  -s      -fi hg38.fa  -fullHeader   -bed TP.bed
-
-
-
